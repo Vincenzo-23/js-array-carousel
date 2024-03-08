7da219ce951fbb6381a3f8c87a3be51ceeda4974
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-08 15:57:15 +0100

    FIX: fixed the div with class prev and next in the html and their css style. ADD: added the images array in the js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,3 @@
-console.log("inizio")
+//dichiaro un array con all'interno delle stringhe formate dal percorso delle immagini
+
+const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"]

6668092535a7fba48360851d79dfa669c78fcaf4
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-08 16:23:02 +0100

    Wrote the first part of the pseudocode on js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-//dichiaro un array con all'interno delle stringhe formate dal percorso delle immagini
+//dichiaro un array con all'interno le stringhe formate dal percorso delle immagini
 
-const images = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"]
+const imagesArray = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"]; //array[string]
+
+//recupero dal DOM l'elemento con classe items_container
+//inserisco all'interno di questo elemento le varie immagini
+    //utilizzo un ciclo for per prendere ogni singolo elemento dell'imagesArray
+    //per ognuno di questo elemento dell'array creo una struttura html
+    //inserico questa struttura appena creata, all'interno dell'items_container nell html concatenandola
+
+
+    

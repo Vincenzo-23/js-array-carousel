711ccdf0885547d4021a9e43ef32a68aaf041203
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-08 16:53:22 +0100

    ADD: added the for cycle to take each element of the imagesArray and insert them in the html, inside the items_container, with the innerhtml property

--- js/app.js
+++ js/app.js
@@ -2,11 +2,25 @@
 
 const imagesArray = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/04.webp", "./img/05.webp"]; //array[string]
 
-//recupero dal DOM l'elemento con classe items_container
-//inserisco all'interno di questo elemento le varie immagini
-    //utilizzo un ciclo for per prendere ogni singolo elemento dell'imagesArray
-    //per ognuno di questo elemento dell'array creo una struttura html
-    //inserico questa struttura appena creata, all'interno dell'items_container nell html concatenandola
 
+//recupero dal DOM l'elemento con classe items_container con un queryselector tramite la classe
+    const itemsContainerDOMELEMENT = document.querySelector(".items_container")
 
+//inserisco all'interno di questo elemento (itemsContainerDOMELEMENT) le varie immagini
+    //utilizzo un ciclo for per iterare ogni singolo elemento dell'imagesArray    
+    for (let index = 0; index < imagesArray.length; index++){
     
+    //dichiaro una const alla quale assegno il valore del singolo elemento dell'imagesArray per semplificarmi il lavoro    
+        const imageSRC = imagesArray[index];
+        
+    //Dichiaro una const dove inserisco la struttura html che mi serve, dove vado quindi ad inserire all'interno del template literal
+    //la const precedentemente creata (imageSRC) con all'interno il valore del singolo elemento dell'imagesArray
+        const sliderItemHTML = `
+        <div class="item">
+            <img src="${imageSRC}" alt="">
+        </div>
+        `
+    //inserico questa struttura html appena creata, all'interno dell'items_container nell html stesso, concatenandola        
+        itemsContainerDOMELEMENT.innerHTML += sliderItemHTML;
+
+    }

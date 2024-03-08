8bdfd8de7f7090c8c882e6d918a8c9f549204c16
Vincenzo-23 <specialevincenzo88@gmail.com>
2024-03-08 18:12:15 +0100

    ADD: added the js part in which i selected the first image inside the item container adding it the class "active". Added the event listener "click"  on the element with class "next".

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ const imagesArray = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/0
 
 
 //recupero dal DOM l'elemento con classe items_container con un queryselector tramite la classe
-    const itemsContainerDOMELEMENT = document.querySelector(".items_container")
+const itemsContainerDOMELEMENT = document.querySelector(".items_container")
 
 //inserisco all'interno di questo elemento (itemsContainerDOMELEMENT) le varie immagini
     //utilizzo un ciclo for per iterare ogni singolo elemento dell'imagesArray    
-    for (let index = 0; index < imagesArray.length; index++){
+for (let index = 0; index < imagesArray.length; index++){
     
     //dichiaro una const alla quale assegno il valore del singolo elemento dell'imagesArray per semplificarmi il lavoro    
         const imageSRC = imagesArray[index];
@@ -23,4 +23,36 @@ const imagesArray = ["./img/01.webp", "./img/02.webp", "./img/03.webp", "./img/0
     //inserico questa struttura html appena creata, all'interno dell'items_container nell html stesso, concatenandola        
         itemsContainerDOMELEMENT.innerHTML += sliderItemHTML;
 
-    }
+}
+
+
+//procedo a selezionare la prima immagine per poi assegnargli la classe active
+
+    //dichiaro una const e recupero tutti i div con classe item tramite getElementsByClassName
+const divItemList = document.getElementsByClassName("item") //HTMLCollection che è una sorta di array
+
+    //dichiaro una variabile alla quale assegno 0 quindi un valore number (che potrò quindi incrementare)
+    //da assegnare come indice al div che riceverà la classe active
+let activeItemIndex = 0;
+
+  
+divItemList[activeItemIndex].classList.add("active");
+    
+
+//procedo a recuperare dal DOM l'elemento con classe next tramite query selector
+const next = document.querySelector(".next")
+
+//resto all'ascolto del click su questo elemento e faccio si che ad ogni click la classe active verrà assegnata al div successivo
+//(andando quindi ad incrementare di 1 l'indice assegnato a divItemList quindi l'activeItemIndex)
+next.addEventListener("click", function(){
+    
+    //rimuovo la classe active dall'elemento corrente. L'elemento corrente sarà quello che attualmente avrà la classe active
+    divItemList[activeItemIndex].classList.remove("active")
+
+    //incremento activeItemIndex di 1
+    activeItemIndex++;
+    console.log(activeItemIndex)
+    //aggiungo la classe active al nuovo elemento
+    divItemList[activeItemIndex].classList.add("active");
+    console.log(divItemList[activeItemIndex])
+})
